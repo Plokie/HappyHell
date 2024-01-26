275dff08705a32003bf2e21b68b56e4e8f37ec65
zylo_dev <70657892+zylonity@users.noreply.github.com>
2024-01-26 21:03:33 +0000

    Started playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
         
+    [SerializeField, Range(0, 10)]
+    private float playerSpeed = 2.0f;
+
+    [SerializeField, Range(0, 10)]
+    private float jumpHeight = 1.0f;
+    
+    [SerializeField, Range(-20, 0)]
+    private float gravityValue = -9.81f;
+
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
