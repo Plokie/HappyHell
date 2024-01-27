576a8cd4fadf785a26ea58f98b13e32b0bda9c96
zylo_dev <kkhaleelkk505@gmail.com>
2024-01-27 23:22:04 +0000

    Added happy and hell health bars

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.Animations;
+using UnityEngine.UI;
 
-public class NPC : MonoBehaviour
+public class NPC : MonoBehaviour, IShootable
 {
 
     public Animator npcHappy, npcHell;
     public bool Alive = true;
 
-    
+    int Health = 100;
+
+    void IShootable.BeingShot()
+    {
+        
+    }
+
+    void IShootable.StartShot()
+    {
+
+    }
+
     void Update()
     {
         if (!Alive){
             npcHappy.SetBool("Alive", false);
             npcHell.SetBool("Alive", false);
         }
+        
     }
 }
+
