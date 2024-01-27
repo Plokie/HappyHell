88d82845e56cce80134d09691e8c3dfcef125fb0
zylo_dev <kkhaleelkk505@gmail.com>
2024-01-27 19:38:12 +0000

    NPCs added

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+
+    public bool Alive = true;
+    public bool Hell = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
