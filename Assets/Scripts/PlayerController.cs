03f1931b258803c1f7d4cf495aed5e75a8202fd7
zylo_dev <kkhaleelkk505@gmail.com>
2024-01-27 14:19:45 +0000

    Added breathing and bobble walk to playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cameraTransform;
+    public CinemachineVirtualCamera vcam;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
         
     [SerializeField, Range(0, 10)]
-    private float playerSpeed = 2.0f;
+    private float playerSpeed = 5.0f;
 
     [SerializeField, Range(0, 20)]
-    private float jumpHeight = 1.0f;
+    private float jumpHeight = 5.0f;
     
     [SerializeField, Range(-20, 0)]
     private float gravityValue = -9.81f;
@@ -22,15 +24,12 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-
+        
     }
 
     void Update()
     {
         gameObject.transform.rotation = new Quaternion(gameObject.transform.rotation.x, cameraTransform.rotation.y, gameObject.transform.rotation.z, gameObject.transform.rotation.w);
-
-        //print(playerVelocity.y);
-        //print(groundedPlayer);
 
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer)
@@ -44,6 +43,16 @@ public class PlayerController : MonoBehaviour
         float fwdSpeed = playerSpeed * Input.GetAxis("Vertical");
         float sdeSpeed = playerSpeed * Input.GetAxis("Horizontal");
         controller.SimpleMove((forward * fwdSpeed) + -(side * sdeSpeed));
+
+        if(fwdSpeed > 0.1){
+            vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 1 + (fwdSpeed / 4);
+            vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = 1 + fwdSpeed + 0.5f;
+        }
+        else{
+            vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 1;
+            vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = 1;
+        }
+
 
         if (Input.GetButtonDown("Jump") && groundedPlayer)
         {
