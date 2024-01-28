2429f341a4e5d4c98b698eb93a201355833bf49e
Plokie <80136737+Plokie@users.noreply.github.com>
2024-01-28 02:38:02 +0000

    only pickup ammo when 0

--- Assets/Scripts/CanisterPickup.cs
+++ Assets/Scripts/CanisterPickup.cs
@@ -9,6 +9,10 @@ public class CanisterPickup : MonoBehaviour, IPickup
     [SerializeField] float spinSpeed = 15f;
     [SerializeField] float levitateFrequency = 1f;
     [SerializeField] float levitateAmplitude = 0.1f;
+    Gun gun;
+    void Start() {
+        gun = GameObject.FindGameObjectWithTag("PlayerCamTransform").GetComponentInChildren<Gun>();
+    }
     void Update() {
         modelTransform.Rotate(Vector3.up, spinSpeed * Time.deltaTime, Space.Self);
         modelTransform.localPosition = new Vector3(modelTransform.localPosition.x, 0.45f + (Mathf.Sin(Time.time * levitateFrequency) * levitateAmplitude), modelTransform.localPosition.z);
@@ -17,9 +21,12 @@ public class CanisterPickup : MonoBehaviour, IPickup
         modelTransform.gameObject.SetActive(Sanity.Instance.IsSane);
     }
     void IPickup.OnPickup() {
-        print("Pickup canister");
-        Destroy(gameObject);
+        if(gun.Ammo <= 0.0f) {
+            print("Pickup canister");
+            Destroy(gameObject);
 
-        GameObject.FindGameObjectWithTag("PlayerCamTransform").GetComponentInChildren<Gun>().ReplenishAmmo();
+            gun.ReplenishAmmo();
+        }
+
     }
 }
